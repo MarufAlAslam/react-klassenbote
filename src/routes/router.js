495a757e8c94263e28bf9a3a8302f907baa1b670
marufalaslam <maruf14@cse.pstu.ac.bd>
2024-01-14 14:03:54 +0600

    router initiated

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,8 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layouts/main.layout";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: "Home",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <h1>Home</h1>,
+      },
+      {
+        path: "/about",
+        element: <h1>About</h1>,
+      },
+    ],
   },
 ]);
