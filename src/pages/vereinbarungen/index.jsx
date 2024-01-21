48e7232b653e9bd4338c5d293b6d8f01652a2796
marufalaslam <maruf14@cse.pstu.ac.bd>
2024-01-21 18:04:50 +0600

    vereinbarungen doe

--- src/pages/vereinbarungen/index.jsx
+++ src/pages/vereinbarungen/index.jsx
@@ -9,7 +9,7 @@ const Vereinbarungen = () => {
         <h2 className="text-xl font-bold">Vereinbarungen</h2>
       </div>
 
-      <div className="mt-6 mb-4 p-8 bg-white rounded-[8px]">
+      <div className="mt-6 mb-4 md:p-8 p-3 bg-white rounded-[8px]">
         <form className="lg:w-5/12 md:w-8/12 w-full">
           <div className="input-holder rounded-[6px] overflow-hidden bg-gray-100 flex justify-between items-center">
             <p className="text-[12px] py-3 text-left w-[200px] px-3">
@@ -22,43 +22,47 @@ const Vereinbarungen = () => {
           </div>
         </form>
 
-        <table className="table mt-10 w-full mb-[200px]">
-          <thead>
-            <th className="border-b text-sm p-3 text-left">Datenname</th>
-            <th className="border-b text-sm p-3 text-left">Größe</th>
-            <th className="border-b text-sm p-3 text-right">Gespeichert am</th>
-          </thead>
-          <tbody>
-            <tr>
-              <td className="border-b text-sm p-3 text-left">
-                <Link to="/dokumenten-bibliotek/1">
-                  Vertragsunterlagen_SCHULE_20.05.2023.pd
-                </Link>
-              </td>
-              <td className="border-b text-sm p-3 text-left">2,45 MB</td>
-              <td className="border-b text-sm p-3 text-left">
-                <div className="flex text-sm justify-end items-center gap-2">
-                  <span>01.01.2024</span>
-                  <FaCog className="text-blue-400" />
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <td className="border-b text-sm p-3 text-left">
-                <Link to="/dokumenten-bibliotek/2">
-                  Datenschutzerklärung_SCHULE_UNTERSCHRIEBEN.pdf
-                </Link>
-              </td>
-              <td className="border-b text-sm p-3 text-left">2,45 MB</td>
-              <td className="border-b text-sm p-3 text-left">
-                <div className="flex text-sm justify-end items-center gap-2">
-                  <span>01.01.2024</span>
-                  <FaCog className="text-blue-400" />
-                </div>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+        <div className="overflow-x-auto">
+          <table className="table mt-10 md:w-full w-[180%] mb-[200px]">
+            <thead>
+              <th className="border-b text-sm p-3 text-left">Datenname</th>
+              <th className="border-b text-sm p-3 text-left">Größe</th>
+              <th className="border-b text-sm p-3 text-right">
+                Gespeichert am
+              </th>
+            </thead>
+            <tbody>
+              <tr>
+                <td className="border-b text-sm p-3 text-left">
+                  <Link to="/dokumenten-bibliotek/1">
+                    Vertragsunterlagen_SCHULE_20.05.2023.pdf
+                  </Link>
+                </td>
+                <td className="border-b text-sm p-3 text-left">2,45 MB</td>
+                <td className="border-b text-sm p-3 text-left">
+                  <div className="flex text-sm justify-end items-center gap-2">
+                    <span>01.01.2024</span>
+                    <FaCog className="text-blue-400" />
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td className="border-b text-sm p-3 text-left">
+                  <Link to="/dokumenten-bibliotek/2">
+                    Datenschutzerklärung_SCHULE_UNTERSCHRIEBEN.pdf
+                  </Link>
+                </td>
+                <td className="border-b text-sm p-3 text-left">2,45 MB</td>
+                <td className="border-b text-sm p-3 text-left">
+                  <div className="flex text-sm justify-end items-center gap-2">
+                    <span>01.01.2024</span>
+                    <FaCog className="text-blue-400" />
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
 
       <div className="copyright pt-5 text-center">
